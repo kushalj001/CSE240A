99fce2c11327ee09279f19c3442b6d21d4db03cf
kushalj001 <kushalj001@gmail.com>
2022-11-24 16:35:14 -0800

    added tournament

--- src/predictor.c
+++ src/predictor.c
@@ -36,10 +36,23 @@ int verbose;
 //
 //TODO: Add your own Branch Predictor data structures here
 //
-int ghistoryMask;
-int pcMask;
-int *gshareTable;
-
+// gshare
+uint32_t ghistoryMask;
+uint32_t pcMask;
+uint32_t *gshareTable;
+uint32_t globalHistory;
+// tournament
+uint32_t *globalHistoryTable;
+// indexed by global history bits and records the common case of global branches
+uint32_t *localHistoryTable;
+// indexed by local history bits which are calculated using pc and local branch table. Records
+// common case of the branch under consideration
+uint32_t *choiceTable;
+// used to choose if the global prediction is correct or local. If upon indexing with global history
+// bits, the prediction is taken (strong or weak), use the pred from the global table, else from local
+uint32_t *localBranchTable;
+// records the local history of branches, indexed by pc bits
+uint32_t lhistoryMask;
 //------------------------------------//
 //        Predictor Functions         //
 //------------------------------------//
@@ -52,29 +65,70 @@ init_predictor()
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
+  globalHistory = 0;
+  
   switch(bpType){
     case GSHARE:
+      
       // ghistoryBits = 8; 
       // keeps track of global history; 01101101 where 0 means a branch was not taken and vice-versa
       // pcIndexBits = 8;
       // PC bits used to access the BHT for a particular branch; same as ghistoryBits for gshare
 
-      ghistoryMask = 1 << ghistoryBits - 1;
+      ghistoryMask = (1 << ghistoryBits) - 1;
       // 2^ghistoryBits - 1. Works as a modulo operator to make sure that any result lies in the
       // range [0, 2^ghistoryBits-1]. This directly translates to the number of entries in the 
       // table. For instance with ghistoryBits = 8, there will be 256 entries indexed from 0 to 255.
       // As such to index this table, we need to ensure that the index lies in the same range. This
       // can achieved by &ing the result with ghistoryMask.
-      pcMask = ghistoryMask;
       int numEntries = 1 << ghistoryBits;
-      gshareTable = (int*)malloc(sizeof(int) * numEntries);
+      gshareTable = (uint32_t*)malloc(sizeof(uint32_t) * numEntries);
       // BHT of size 4 bytes * number of entries.
       // This table records the 2-bit saturating counters of various branches.
       for(int i=0;i<numEntries;i++) {
           gshareTable[i] = 1;
           // initialize all the 2 bit counters to 1 which is weakly not taken.
       }
+      
+    case TOURNAMENT:
+      //define mask, size of table and then table; init table
+      {
+        int globalTableSize;
+        ghistoryMask = (1 << ghistoryBits) - 1;
+        globalTableSize = 1 << ghistoryBits;
+        globalHistoryTable = (int*) malloc(sizeof(int) * globalTableSize);
+        for(int i=0;i<globalTableSize;i++){
+          globalHistoryTable[i] = 1;
+        }
 
+        int localTableSize;
+        lhistoryMask = (1 << lhistoryBits) - 1;
+        localTableSize = 1 << lhistoryBits;
+        localHistoryTable = (int*)malloc(sizeof(int) * localTableSize);
+        for(int i=0;i<localTableSize;i++){
+          localHistoryTable[i] = 1;
+        }
+
+        int localBranchTableSize;
+        pcMask = (1 << pcIndexBits) - 1;
+        localBranchTableSize = 1 << pcIndexBits;
+        localBranchTable = (int*)malloc(sizeof(int) * localBranchTableSize);
+        for(int i=0;i<localBranchTableSize;i++){
+          localBranchTable[i] = 0;
+        }
+
+        int choiceTableSize;
+        choiceTableSize = 1 << ghistoryBits;
+        choiceTable = (int*)malloc(sizeof(int) * choiceTableSize);
+        for(int i=0;i<choiceTableSize;i++){
+          choiceTable[i] = 2;
+          // weakly taken for global pred
+        }
+
+      }
+
+    default:
+      break;
   }
   
 
@@ -90,34 +144,60 @@ make_prediction(uint32_t pc)
   //
   //TODO: Implement prediction scheme
   //
-
+  
   // Make a prediction based on the bpType
   switch (bpType) {
-
-    // for gshare
-    int pred;
-    int ghistBits;
-    int pcBits;
-    int index;
 
     case STATIC:
       return TAKEN;
     case GSHARE:
-      ghistBits = ghistoryBits & ghistoryMask;
-      pcBits = pc & pcMask;
-      index = ghistBits ^ pcBits;
-      pred = gshareTable[index];
-      if(pred > 1){
-        return TAKEN;
-      }
-      else {
-        return NOTTAKEN;
-      }
+      {
+        uint32_t pred;
+        uint32_t ghistBits;
+        uint32_t pcBits;
+        uint32_t index;
 
+        ghistBits = globalHistory & ghistoryMask;
+        pcBits = pc & ghistoryMask;
+        index = ghistBits ^ pcBits;
+        pred = gshareTable[index];
+        if(pred > 1){
+          return TAKEN;
+        }
+        else {
+          return NOTTAKEN;
+        }
+      }
     case TOURNAMENT:
+      
+      {
+        uint32_t pred;
+        uint32_t ghistBits;
+        uint32_t pcBits;
+        uint32_t lhistBits;
+        uint32_t choiceTablePred;
+        ghistBits = globalHistory & ghistoryMask;
+        choiceTablePred = choiceTable[ghistBits];
+        if(choiceTablePred > 1){
+          pred = globalHistoryTable[ghistBits];
+        }
+        else{
+          pcBits = pc & pcMask;
+          lhistBits = localBranchTable[pcBits] & lhistoryMask;
+          pred = localHistoryTable[lhistBits];
+        }
+
+        if(pred > 1){
+          return TAKEN;
+        }
+        else{
+          return NOTTAKEN;
+        }
+      }
     case CUSTOM:
     default:
       break;
+    
   }
 
   // If there is not a compatable bpType then return NOTTAKEN
@@ -134,27 +214,94 @@ train_predictor(uint32_t pc, uint8_t outcome)
   //
   //TODO: Implement Predictor training
   //
+  
+
   switch(bpType){
     case GSHARE:
-      int ghistBits;
-      int pcBits;
-      int index;
-      ghistBits = ghistoryBits & ghistoryMask;
-      pcBits = pc & pcMask;
+    {
+      uint32_t ghistBits;
+      uint32_t pcBits;
+      uint32_t index;
+      ghistBits = globalHistory & ghistoryMask;
+      pcBits = pc & ghistoryMask;
       index = ghistBits ^ pcBits;
       // recalculate index to update the BHT
 
       if(outcome == TAKEN && gshareTable[index]<3){
-        gshareTable[index] += 1;
+        gshareTable[index]++;
       } else {
         if(outcome == NOTTAKEN && gshareTable[index]>0){
-          gshareTable[index] -= 1;
+          gshareTable[index]--;
         }
       }
       // update saturating counters based on the true outcome and the current state of the counter
-      ghistoryBits = ghistoryBits << 1 | outcome; 
+      globalHistory = globalHistory << 1 | outcome;
       // append new outcome to the history
       return;
+    }
+    case TOURNAMENT:
+      
+      {
+        uint32_t ghistBits;
+        uint32_t pcBits;
+        uint32_t lhistBits;
+        uint32_t globalTablePred;
+        uint32_t localTablePred;
+        uint32_t choiceTablePred;
+        ghistBits = globalHistory & ghistoryMask;
+        choiceTablePred = choiceTable[ghistBits];
+        globalTablePred = globalHistoryTable[ghistBits];
+
+
+        if(globalTablePred > 1){
+          globalTablePred = TAKEN;
+        } else{
+          globalTablePred = NOTTAKEN;
+        }
+
+        pcBits = pc & pcMask;
+        lhistBits = lhistoryMask & localBranchTable[pcBits];
+        localTablePred = localHistoryTable[lhistBits];
+
+        if(localTablePred > 1){
+          localTablePred = TAKEN;
+        } else{
+          localTablePred = NOTTAKEN;
+        }
+
+        if(outcome==globalTablePred && outcome!=localTablePred && choiceTablePred!=3){
+          choiceTable[ghistBits]++;
+        } else{
+          if(outcome==localTablePred && outcome!=globalTablePred && choiceTablePred!=0){
+            choiceTable[ghistBits]--;
+          }
+        }
+
+        if(outcome==TAKEN){
+          if(globalHistoryTable[ghistBits]!=3){
+            globalHistoryTable[ghistBits]++;
+          }
+          if(localHistoryTable[lhistBits]!=3){
+            localHistoryTable[lhistBits]++;
+          }
+        }else{
+          if(globalHistoryTable[ghistBits]!=0){
+            globalHistoryTable[ghistBits]--;
+          }
+          if(localHistoryTable[lhistBits]!=0){
+            localHistoryTable[lhistBits]--;
+          }
+        }
+
+        globalHistory = globalHistory << 1 | outcome;
+        globalHistory = globalHistory & ghistoryMask;
+        localBranchTable[pcBits] = localBranchTable[pcBits] << 1 | outcome & lhistoryMask;
+        return;
+      }
+
+    default:
+      break;
+
   }
   
 }
